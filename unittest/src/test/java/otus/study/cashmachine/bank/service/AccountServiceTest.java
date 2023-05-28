8f9521bd7a5a38eb241a93395c6b7c6e3284e99a
Anton Sokolov <75442929+SokolovAnton94@users.noreply.github.com>
2023-05-28 13:33:22 +0300

    Unit test homework (#2)
    done

--- unittest/src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ unittest/src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -2,23 +2,43 @@ package otus.study.cashmachine.bank.service;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.data.Account;
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
 
+import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
-
+    @Mock
     AccountDao accountDao;
 
     AccountServiceImpl accountServiceImpl;
 
     @Test
     void createAccountMock() {
+        accountServiceImpl = new AccountServiceImpl(accountDao);
+        Account testAccount = new Account(1L, BigDecimal.TEN);
+        when(accountDao.saveAccount(any())).thenReturn(testAccount);
+        assertEquals(testAccount, accountServiceImpl.createAccount(BigDecimal.TEN));
 // @TODO test account creation with mock and ArgumentMatcher
     }
 
     @Test
     void createAccountCaptor() {
+        accountServiceImpl = new AccountServiceImpl(accountDao);
+        ArgumentCaptor<Account> accountCaptor = ArgumentCaptor.forClass(Account.class);
+        when(accountDao.saveAccount(accountCaptor.capture())).thenCallRealMethod();
+        Account resultAccount = accountServiceImpl.createAccount(BigDecimal.ONE);
+        assertEquals(0, accountCaptor.getValue().getAmount().compareTo(resultAccount.getAmount()));
 //  @TODO test account creation with ArgumentCaptor
     }
 
