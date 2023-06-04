b8b8e92392e912731bf59bb1bed4432bf72087aa
Anton Sokolov <Anton_Sokolov@epam.com>
2023-06-04 14:30:16 +0300

    Revert "As a result, the toString() method is generated in the source class, not in the compiled one. I haven't found a way to fix it)"
    This reverts commit 6b41db9d1abf1c7e6f38cb884951fcbadd5c79d0.

--- annotation-processor/src/main/java/ru/otus/homework/anton/sokolov/Main.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.homework.anton.sokolov;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
--- custom-to-string/src/main/java/ru/otus/homework/anton/sokolov/test/TestClass.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework.anton.sokolov.test;
-
-import ru.otus.homework.anton.sokolov.annotation.CustomToString;
-
-@CustomToString
-public class TestClass {
-
-}
